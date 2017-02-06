e67444ace6b7dc232bd0bc8a09d34038d11b6561
joslcarp <joslcarp@ut.utm.edu>
2017-02-05 20:42:12 -0600

    chart.php
    --Created chart.php as a single file consisting of both chart.htm and
    work.php
    --Added required fields and error class (error text color) for chart
    name and chart data
    --Added htmlspcialchars() funtion for secure form transfer.  Prevent
    malicious code inserted into php variables.
    --echo all chart variables
    
    --To do next:
    1. Seperate chartData into sub strings using explode()
    2.  Finish part 1 echoes: highest/lowest/average score, score sorting
    
    Reference for form validation:
    http://www.w3schools.com/php/php_form_validation.asp

---
+++ chart.php
@@ -0,0 +1,83 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>SVG Chart Generator</title>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>
+
+<?php
+// define variables and set to empty values
+$nameErr = $chartDataErr = "";
+$name = $chartType = $sortBy = $chartData = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+  }
+
+  $chartType = test_input($_POST["chartType"]);
+  $sortBy = test_input($_POST["sortBy"]);
+
+  if (empty($_POST["chartData"])) {
+    $chartDataErr = "Chart Data is required";
+  } else {
+    $chartData = test_input($_POST["chartData"]);
+  }
+
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+
+?>
+
+<h2>SVG Chart Generator</h2>
+<p><span class="error">* required field.</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+  Chart Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  Chart Type: <select name="chartType" value="<?php echo $chartType;?>">
+    <option value="stats">Stats</option>
+    <option value="bar">Bar</option>
+    <option value="line">Line</option>
+    <option value="pie">Pie</option>
+  </select>
+  <br><br>
+  Sort by: <select name="sortBy" value="<?php echo $sortBy;?>">
+    <option value="none">None</option>
+    <option value="score">Score</option>
+    <option value="name">Name</option>
+    <option value="lastName">Last Name</option>
+  </select>
+  <br><br>
+  Chart Data: <textarea name="chartData" rows="5" cols="40"><?php echo $chartData;?></textarea>
+  <span class="error">* <?php echo $chartDataErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $chartType;
+echo "<br>";
+echo $sortBy;
+echo "<br>";
+echo $chartData;
+echo "<br>";
+?>
+
+</body>
+</html>
